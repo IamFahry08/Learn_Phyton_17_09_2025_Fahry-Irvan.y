3977c576213abdb635a634a455bb6b38de1d5cda
Iam_Fahry08 <154793805+IamFahry08@users.noreply.github.com>
2025-09-17 10:11:04 +0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,18 @@
+nilai = int(input("Masukkan Nilainya..?"))
+
+if 95 <= nilai <= 100:
+    print("Good job, kamu dapat nilai 'A'")
+elif 80 <= nilai <= 94:
+    print("oke, kamu dapat nilai 'B'")
+elif 70 <= nilai <= 79:
+    print("Oke lah, kamu dapat nilai 'C'")
+elif 50 <= nilai <= 69:
+    print("tingkatkan lagi, kamu dapat nilai 'D'")
+elif 0 <= nilai <= 49:
+    print("Ayo belajar lagi 'E'")
+else:
+    print("Masukkan nilai dari 0 sampai 100")
+    
+    
+
+
